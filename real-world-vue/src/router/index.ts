6321d4c2d63f6991cff13aabb347375b91ca1608
thanap151255 <thanap151255@gmail.com>
2023-08-06 22:48:27 +0700

    4.2

--- real-world-vue/src/router/index.ts
+++ real-world-vue/src/router/index.ts
@@ -8,6 +8,7 @@ import EventEditView from "../views/EventEditView.vue"
 import EventRegisterView from "../views/EventRegisterView.vue"
 import EventLayoutView from "../views/EventLayoutView.vue"
 import NotFoundView from "../views/NotFoundView.vue"
+import NetworkErrorView from '../views/NetworkErrorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -63,6 +64,17 @@ const router = createRouter({
       path : '/:catchAll(.*)'
     , name : 'not-found'
     ,component : NotFoundView
+  },
+  {
+    path: '/404/:resource',
+    name: '404-resource',
+    component: NotFoundView,
+    props: true
+  },
+  {
+    path : '/network-error',
+    name : 'network-error',
+    component : NetworkErrorView
     }
   ]
 })
